69b38ca20bc164b9bf7993b7b58e961c7176c129
widekuan <widekuan@outlook.com>
2015-05-05 16:51:07 +0800

    Now I can sing
    Now I can sing~

--- app/src/main/java/bob/sun/mpod/service/PlayerService.java
+++ app/src/main/java/bob/sun/mpod/service/PlayerService.java
@@ -5,11 +5,21 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 
+import java.io.IOException;
+
 /**
  * Created by sunkuan on 15/4/29.
  */
 public class PlayerService extends Service implements MediaPlayer.OnCompletionListener {
     private MediaPlayer mediaPlayer;
+
+    public static final int CMD_PLAY = 1;
+    public static final int CMD_PAUSE = 2;
+    public static final int CMD_STOP = 4;
+    public static final int CMD_NEXT = 5;
+    public static final int CMD_PREVIOUS = 6;
+    public static final int CMD_VOLUMN_UP = 7;
+    public static final int CMD_VOLUMN_DOWN = 8;
 
     @Override
     public void onCreate(){
@@ -25,6 +35,26 @@ public class PlayerService extends Service implements MediaPlayer.OnCompletionLi
     }
     @Override
     public int onStartCommand(Intent intent, int flags,int startId){
+        switch (intent.getIntExtra("CMD",-1)){
+            case CMD_PLAY:
+                String fileName = intent.getStringExtra("DATA");
+                mediaPlayer.reset();
+                try {
+                    mediaPlayer.setDataSource(fileName);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case CMD_PAUSE:
+                if (!mediaPlayer.isPlaying()){
+                   mediaPlayer.pause();
+                }
+                break;
+            default:
+                break;
+        }
 
         return super.onStartCommand(intent, flags, startId);
     }
