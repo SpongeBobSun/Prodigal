d3cae969efebc6c48037b7a50f8125b409d782fe
bob.sun <widekuan@outlook.com>
2018-03-20 14:02:38 +0800

    add gh-pages

--- app/src/main/aidl/bob/sun/bender/model/SongBean.aidl
+++
@@ -1,5 +0,0 @@
-// SongBean.aidl
-package bob.sun.bender.model;
-
-// Declare any non-default types here with import statements
-parcelable SongBean;
--- libraries/material-intro/src/main/java/com/heinrichreimersoftware/materialintro/slide/RestorableSlide.java
+++
@@ -1,7 +0,0 @@
-package com.heinrichreimersoftware.materialintro.slide;
-
-import android.support.v4.app.Fragment;
-
-public interface RestorableSlide extends Slide {
-    void setFragment(Fragment fragment);
-}
